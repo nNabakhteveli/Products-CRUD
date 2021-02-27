c24ae80bc59bf1285ae558b7fd87fb4d39d61064
Nabakhteveli <nabakhtevelin@gmail.com>
2021-02-28 00:19:33 +0400

    Adding 'add product' page

---
+++ create.php
@@ -0,0 +1,52 @@
+<?php 
+
+$pdo = new PDO('mysql:host=localhost;port=3308;dbname=products_crud', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo "<pre>";
+var_dump($_GET);
+echo "</pre>";
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Products CRUD</title>
+  </head>
+  <body>
+
+  <h1>Create new Product</h1>
+    <form>
+    <div class="mb-3">
+        <label>Title</label>
+        <input name='title' type="name" class="form-control">
+        <div class="form-text">Enter your product name</div>
+    </div>
+    <div class="mb-3">
+        <label>Product Price</label>
+        <div class="form-text"></div>
+        <input name='price' type="number" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Description</label>
+        <div class="form-text"></div>
+        <textarea name='description' type="text" cols="60" rows="5"></textarea>
+    </div>
+    <div class="mb-3">
+        <label>Image</label><br>
+        <input type="file" name='image'>
+        <div id="emailHelp" name='image' class="form-text"></div>
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -17,12 +17,17 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+   
+    <link rel="stylesheet" href="style.css">
     <title>Products CRUD</title>
   </head>
-
+  
   <body>
   <h1>Products CRUD</h1>
 
+<p>
+    <a href="./create.php"><button type="button" class="btn btn-outline-success">Add Product</button></a>
+</p>
   <table class="table">
   <thead>
     <tr>
