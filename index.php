56ec77cd0b84b4c5c44f5dfba734e5741ba50ece
Nabakhteveli <nabakhtevelin@gmail.com>
2021-02-27 23:37:48 +0400

    basic look

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
+<?php 
+
+$pdo = new PDO('mysql:host=localhost;port=3308;dbname=products_crud', 'root', '');
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$statement = $pdo->prepare('SELECT * FROM products');
+$statement->execute();
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
@@ -14,30 +26,28 @@
   <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Image</th>
+      <th scope="col">Title</th>
+      <th scope="col">Price</th>
+      <th scope="col">Create Date</th>
+      <th scope="col">Actions</th>
     </tr>
   </thead>
   <tbody>
+  <?php foreach ($products as $i => $product) { ?>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+
+      <th scope="row"><?php echo $i + 1 ?></th>
+      <td></td>
+      <td><?php echo $product['title']?></td>
+      <td><?php echo $product['price']?></td>
+      <td><?php echo $product['create_date']?></td>
+      <td>
+      <button type="button" class="btn btn-outline-primary">Edit</button>
+      <button type="button" class="btn btn-outline-danger">Delete</button>
+      </td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+  <?php } ?>
   </tbody>
 </table>
   </body>
